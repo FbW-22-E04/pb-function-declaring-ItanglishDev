0cfefdbf3dbac607ad757c691e5cce47d677038c
chris <chrishethertonweb@gmail.com>
2022-07-13 14:33:03 +0200

    ex 3 done

--- solution.js
+++ solution.js
@@ -8,3 +8,24 @@
 const multiplyNumberByAnother = (a, b) => console.log("The result is", a * b);
 
 multiplyNumberByAnother(2, 3);
+
+function multiplyNumberByAnotherB(a, b) {
+  console.log("The % are", a % b, "and", b % a);
+}
+
+multiplyNumberByAnotherB(5, 3);
+
+function multiplyNumberByAnotherA(a, b) {
+  let value = a % b;
+  let valueTwo = b % a;
+  console.log("The % are", value, "and", valueTwo);
+}
+
+multiplyNumberByAnotherA(5, 3);
+
+const multiplyNumberByAnotherC = (a, b) => {
+  console.log("The % are", a % b);
+  console.log("and", b % a);
+};
+
+multiplyNumberByAnotherC(5, 3);
