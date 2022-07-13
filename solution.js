9f2a9f6e39aea9ef0f519e001f213ecb0a4fea47
chris <chrishethertonweb@gmail.com>
2022-07-13 14:23:37 +0200

    ex 2 done

--- solution.js
+++ solution.js
@@ -1,6 +1,10 @@
-function multiplyNumberByAnother(a, b) {
-  let value = a * b;
-  console.log(value);
-}
+// function multiplyNumberByAnother(a, b) {
+//   let value = a * b;
+//   console.log(value);
+// }
+
+// multiplyNumberByAnother(2, 3);
+
+const multiplyNumberByAnother = (a, b) => console.log("The result is", a * b);
 
 multiplyNumberByAnother(2, 3);
